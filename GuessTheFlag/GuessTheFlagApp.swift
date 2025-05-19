cbd6d0658892553e67767f5c2479c4e7bf97e319
Dhruva Byrapatna <dhruvajb@gmail.com>
2025-05-19 18:08:01 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Dhruva Jothik Byrapatna on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
